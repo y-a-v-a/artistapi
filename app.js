dd7a0881db45fda9bdec879a31cb01dd3eca7078
Vincent Bruijn <vebruijn@gmail.com>
2014-09-06 14:33:04 +0200

    better layout of code

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(function(req, res, next) {
   req.db.accounts = db.collection('accounts');
   next();
 })
+app.set('env', process.env.NODE_ENV || "development");
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -48,6 +49,7 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
+        console.log(err);
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
--- routes/api.js
+++ routes/api.js
@@ -2,52 +2,54 @@ var express = require('express');
 var router = express.Router();
 var api = require('../lib/artist-api.js');
 
-router.get('/', function(req, res) {
+router.route('/').all(function(req, res) {
     res.redirect('/');
 });
 
-router.get('/register', function(req, res) {
+router.route('/v1').all(function(req, res) {
     res.redirect('/');
 });
 
-router.get('/v1/addartist', function(req, res) {
-    res.redirect('/');
-});
-
-router.get('/v1/getartist', function(req, res) {
-    res.set({
-        'Cache-Control': 'private, max-age=0, no-cache'
-    });
+router.route('/v1/getartist').get(function(req, res, next) {
     var key = req.param('key');
     var data = {};
 
     req.db.accounts.find({ 'key': key }).toArray(function(error, rows) {
         if (rows.length === 1) {
-            api.getArtist(req, function(err, item) {
-                if (err) console.log(err);
-                data.artist = item;
-                data.code = 200;
-                data.message = 'Succes';
-                res.send(data.code, JSON.stringify(data));
-            });
+            next();
         } else {
             data.message = 'Bad request';
             data.code = 400;
             res.send(data.code, JSON.stringify(data));
         }
     });
+}).get(function(req, res) {
+    res.set({
+        'Cache-Control': 'private, max-age=0, no-cache'
+    });
+    var data = {};
+
+    api.getArtist(req, function(err, item) {
+        if (err) console.log(err);
+        data.artist = item;
+        data.code = 200;
+        data.message = 'Succes';
+        res.send(data.code, JSON.stringify(data));
+    });
 });
 
-router.post('/register', function(req, res) {
-    var now = Date.now();
-    var data = {};
-    var fqdn = req.body.fqdn
-    data['fqdn'] = fqdn;
-    data['registered'] = now;
-    data['requests'] = 0;
-    data['key'] = api.getKey(req.body.fqdn, now);
-    data['allowed'] = true;
-    
+router.route('/v1/register').get(function(req, res) {
+    res.redirect('/');
+}).post(function(req, res, next) {
+    if (typeof req.body === 'undefined' ||
+            typeof req.body.fqdn === 'undefined' ||
+            req.body.fqdn.length === 0) {
+        var err = new Error('Empty or missing FQDN');
+        next(err);
+    }
+    next();
+}).post(function(req, res, next) {
+    var fqdn = req.body.fqdn;
     req.db.accounts.find({ 'fqdn': fqdn }).toArray(function(error, rows) {
         if (rows.length > 0) {
             res.render('index', {
@@ -56,18 +58,30 @@ router.post('/register', function(req, res) {
                 key: rows[0].key
             });
         } else {
-            req.db.accounts.save(data, function(error, row) {
-                res.render('index', {
-                    title: 'R.A.A.C.',
-                    message: 'Registered!',
-                    key: row.key
-                });
-            });
+            next();
         }
     });
+}).post(function(req, res) {
+    var now = Date.now();
+    var data = {};
+    data['fqdn'] = req.body.fqdn;
+    data['registered'] = now;
+    data['requests'] = 0;
+    data['key'] = api.getKey(req.body.fqdn, now);
+    data['allowed'] = true;
+
+    req.db.accounts.save(data, function(error, row) {
+        res.render('index', {
+            title: 'R.A.A.C.',
+            message: 'Registered!',
+            key: row.key
+        });
+    });    
 });
 
-router.post('/v1/addartist', function(req, res) {
+router.route('/v1/addartist').get(function(req, res) {
+    res.redirect('/');
+}).post(function(req, res) {
     var name = req.body.artist;
     api.addArtist(req, name, function(err) {
         if (!err) {
